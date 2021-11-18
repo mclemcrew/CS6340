9c2ca1c3d0227ac0f7e712394b820d583e2030b3
Michael <michael.clemens@outlook.com>
2021-11-18 07:10:52 -0700

    Midpoint Merge
    - Created 2 more models (3 & 4) that are used to help better validate the efficiency of the model
    - Added training/testing/validation data files and an trace of the model through this space

--- NLP Project (VR Troopers)/train_model.py
+++ NLP Project (VR Troopers)/train_model.py
@@ -1,9 +1,9 @@
 import spacy # NLP Models
 
 def main():
-    nlp = spacy.load("models/01")
+    nlp = spacy.load("models/03")
     nlp.add_pipe("ner")
-    nlp.to_disk("models/01")
+    nlp.to_disk("models/03")
 
 if __name__ == "__main__":
     main()
